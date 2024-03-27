5536fc9a20be871afc7ac6b2329393db6fd8b876
juratdev <juratdev@gmail.com>
2024-03-27 14:18:09 +0500

    fix: arrow icon class binding

--- src/components/Selects.vue
+++ src/components/Selects.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="cursor-pointer w-64">
     <div
-      :class="open ? 'border-blue-500 text-blue-500' : ''"
+      :class="open ? 'border-blue-500  text-blue-500' : ''"
       @click="openDropDown"
       v-click-outside="closeDrop"
       class="flex hover:border-blue-500 hover:text-blue-500 duration-300 transition items-center justify-between bg-gray-light-200 border-2 select-none rounded-md py-2 px-3"
     >
       <p>{{ selectedText }}</p>
-      <slot name="suffix" :class="open ? 'rotate-90' : 'rotate-0'" />
+      <div
+        class="transition duration-300"
+        :class="open ? 'rotate-90' : 'rotate-0'"
+      >
+        <slot name="suffix" />
+      </div>
     </div>
     <transition>
       <div class="z-10">
